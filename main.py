fd702759cee0dd69cfe0a504b42b3cf099b07673
Matt Post <mattpost@microsoft.com>
2023-02-18 07:27:01 -0600

    swap in acl anthology bibtex

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python3
+
 import argparse
 import requests
 import datetime
+import logging
 import json
 import time
 import tqdm
 
-PAPER_DETAILS_URL = "https://api.semanticscholar.org/graph/v1/paper/REPLACE_ME?fields=title,venue,year,publicationDate,publicationTypes,authors,journal,url,externalIds"
-AUTHOR_DETAILS_URL = "https://api.semanticscholar.org/graph/v1/author/REPLACE_ME?fields=papers,papers.year"
+PAPER_DETAILS_URL = "https://api.semanticscholar.org/graph/v1/paper/{}?fields=title,venue,year,publicationDate,publicationTypes,authors,journal,url,externalIds"
+AUTHOR_DETAILS_URL = "https://api.semanticscholar.org/graph/v1/author/{}?fields=papers,papers.year"
+ANTHOLOGY_TEMPLATE = "https://aclanthology.org/{}.bib"
 
 
 def return_request(request_type: str, request_id: int, first_attempt: bool = True) -> dict:
@@ -18,14 +22,14 @@ def return_request(request_type: str, request_id: int, first_attempt: bool = Tru
     request_id = str(request_id)
 
     if request_type == "paper":
-        response = requests.get(PAPER_DETAILS_URL.replace("REPLACE_ME", request_id))
+        response = requests.get(PAPER_DETAILS_URL.format(request_id))
     elif request_type == "author":
-        response = requests.get(AUTHOR_DETAILS_URL.replace("REPLACE_ME", request_id))
+        response = requests.get(AUTHOR_DETAILS_URL.format(request_id))
 
     if response.status_code != 200:
         if first_attempt:
             print("Error in request, got status code {0}".format(response.status_code))
-            time.sleep(60*5 + 30)
+            time.sleep(10)
             return return_request(request_type, request_id, first_attempt=False)
         else:
             raise Exception(response)
@@ -33,6 +37,7 @@ def return_request(request_type: str, request_id: int, first_attempt: bool = Tru
     time.sleep(1.1)
     data = response.json()
     return data
+
 
 def write(papers: list, cache_path: str):
     # eliminate duplicates
@@ -58,6 +63,8 @@ def pull_existing_bibfiles():
         else:
             print("Could not find bibfile at {0}".format(url))
     return bibs
+
+
 def update_cache(cache_path: str, use_existing_cache: bool = False):
     # CLSP faculty
     file_path_authors = "people.json"
@@ -99,11 +106,13 @@ def update_cache(cache_path: str, use_existing_cache: bool = False):
             if paper_dict["paperId"] in paper_ids:
                 # print("skipping paper because we already have it")
                 continue
-            else:
-                paper_details = return_request("paper", paper_dict["paperId"])
+
+            paper_details = return_request("paper", paper_dict["paperId"])
 
             papers.append(paper_details)
+
         papers = write(papers, cache_path)
+
 
 pub_template = \
 """
@@ -126,7 +135,62 @@ def get_year(cache_dict):
         year = None
     return year
 
+
+def get_bibtex(cache_dict):
+    """Generates or retrieves the bibtex entry for the paper
+
+    :param cache_dict: A dictionary of paper metadata obtained from S2.
+    :return: The BibTeX text.
+    """
+    title = cache_dict["title"]
+    journal = cache_dict["venue"]
+    if journal == "" and cache_dict["journal"] is not None and "name" in cache_dict["journal"]:  # maybe a journal
+        journal = cache_dict["journal"]["name"]
+            # print(journal)
+
+    url = cache_dict["url"]
+
+    year = get_year(cache_dict)
+
+    if cache_dict["publicationDate"] is not None:
+        date = datetime.datetime.strptime(cache_dict["publicationDate"], "%Y-%m-%d")
+        month = date.month
+    else:
+        month = None
+
+    author_list = "{" + " and ".join(["{" + item["name"] + "}" for item in cache_dict["authors"]]) + "}"
+    ident = cache_dict["externalIds"]["CorpusId"]
+
+    cur_pub = None
+    if "ACL" in cache_dict["externalIds"]:
+            # Use the Anthology BibTeX if this is a *ACL paper
+        anthology_id = cache_dict["externalIds"]["ACL"]
+        url = ANTHOLOGY_TEMPLATE.format(anthology_id)
+
+            # download the file
+        logging.info(f"-> Swapping in Anthology BibTex for {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            cur_pub = response.text
+
+    if cur_pub is None:
+        # generate this if the Anthology call failed or there was
+        # no Anthology ID
+        cur_pub = pub_template.format(title=title,
+                                          author_list=author_list,
+                                          year=year,
+                                          month="\n\tmonth = {%s}," % month if month is not None else "",
+                                          journal=journal,
+                                          url=url) % ident
+                                      
+    return cur_pub
+
+
 def convert_to_bib(cache_path: str):
+    """Converts the papers.json file to a bibtex file
+
+    :param cache_path: path to the papers.json file    
+    """
     all_pubs = []
     # read json file
     with open(cache_path, "r") as f:
@@ -142,31 +206,7 @@ def convert_to_bib(cache_path: str):
     cache = sorted(cache, key=lambda x: get_year(x), reverse=True)
 
     for cache_dict in cache:
-        title = cache_dict["title"]
-        journal = cache_dict["venue"]
-        if journal == "" and cache_dict["journal"] is not None and "name" in cache_dict["journal"]:  # maybe a journal
-            journal = cache_dict["journal"]["name"]
-            # print(journal)
-
-        url = cache_dict["url"]
-
-        year = get_year(cache_dict)
-
-        if cache_dict["publicationDate"] is not None:
-            date = datetime.datetime.strptime(cache_dict["publicationDate"], "%Y-%m-%d")
-            month = date.month
-        else:
-            month = None
-
-        author_list = "{" + " and ".join(["{" + item["name"] + "}" for item in cache_dict["authors"]]) + "}"
-        ident = cache_dict["externalIds"]["CorpusId"]
-
-        cur_pub = pub_template.format(title=title,
-                                      author_list=author_list,
-                                      year=year,
-                                      month="\n\tmonth = {%s}," % month if month is not None else "",
-                                      journal=journal,
-                                      url=url) % ident
+        cur_pub = get_bibtex(cache_dict)
         all_pubs.append(cur_pub)
 
     with open("references_generated.bib", "w") as fout:
